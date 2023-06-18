48c7f17063294d5219e485755aa30df108018df9
Sivaraman R <sivaraman.ec19@bitsathy.ac.in>
2023-06-18 12:22:16 +0530

    learning bootstrap

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './Style.css'
+function App() {
+  return (
+    <div className=" container text-center d-flex justify-content-center bg-primary ">
+      <div >
+      <h2>Hello Guys start learning bootstrap</h2>
+      <p>I m here to learn bootstrap</p>
+      <button className='btn btn-success'>submit</button>
+      </div>
+     
+      <div>
+      <img src="https://tse2.mm.bing.net/th?id=OIP.l5HK7BRADg8I7e1V8uXphAHaHa&pid=Api&P=0&h=180"></img>
+      </div>
+    </div>
+  );
+}
+
+export default App;
